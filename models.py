b5ccd10f18f9a06147e3d0c6b2d9e4cbce0af1b3
Maruf Ahmed <41258916+mMaRuf1998@users.noreply.github.com>
2026-05-30 21:31:17 +0600

    Create User and Display no hash

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from database import Base
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 
 
-class users(Base):
+class Users(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True)
--- routers/auth.py
+++ routers/auth.py
@@ -1,7 +1,29 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+
+from models import Users
 
 router = APIRouter()
 
-@router.get("/auth")
-async def get_user():
-    return {"username": "admin"}
+class createUserRequest(BaseModel): #You can add validation here using Field/ This is a Pydantic Class/Sample User Input Expectation
+    email:str
+    username:str
+    first_name:str
+    last_name:str
+    password:str
+    is_active:bool
+    role:str
+
+@router.post("/auth")
+async def create_user(newUserRequest:createUserRequest):
+    newUser = Users(
+        email=newUserRequest.email,
+        username=newUserRequest.username,
+        first_name=newUserRequest.first_name,
+        last_name=newUserRequest.last_name,
+        hashed_password=newUserRequest.password,
+        is_active = newUserRequest.is_active,
+        role = newUserRequest.role
+    )
+
+    return newUser

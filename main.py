c63ee76588b45b6ee261c9f90bfb96ba8b750ef5
Maruf Ahmed <41258916+mMaRuf1998@users.noreply.github.com>
2026-06-08 02:03:03 +0600

    Added previously created js and css files

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from starlette.status import HTTP_200_OK
 
 from .models import Base
 from .database import engine
 from .routers import auth,todos,admin,users
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
+templates = Jinja2Templates(directory="TodoApp/templates")
+app.mount("/static", StaticFiles(directory="TodoApp/static"), name="static")
+templates.env.globals["url_for"] = app.url_path_for
+
+
+@app.get("/")
+def test(request: Request):
+    return templates.TemplateResponse({"request": request}, "home.html" )
+
 
 @app.get("/healthy", status_code=HTTP_200_OK)
 def health_check():
